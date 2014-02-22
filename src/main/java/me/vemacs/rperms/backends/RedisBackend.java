eb24accd57c71acd825eedf583180d6617990945
vemacs <gentoo@outlook.com>
2014-02-22 15:08:01 -0700

    moar backend

--- src/main/java/me/vemacs/rperms/backends/RedisBackend.java
+++ src/main/java/me/vemacs/rperms/backends/RedisBackend.java
@@ -1,5 +1,6 @@
 package me.vemacs.rperms.backends;
 
+import com.google.common.base.Joiner;
 import me.vemacs.rperms.data.Group;
 import me.vemacs.rperms.data.PlayerData;
 import me.vemacs.rperms.rPermissions;
@@ -9,14 +10,35 @@ import redis.clients.jedis.Jedis;
 import java.util.*;
 
 public class RedisBackend implements Backend {
-    @Override
-    public void saveGroup(Group group) {
+    private static final Joiner joiner = Joiner.on(",").skipNulls();
 
+    public void saveGroup(Group group) {
+        Jedis jedis = ConnectionManager.getPool().getResource();
+        String name = group.getName().toLowerCase();
+        List<String> perms = new ArrayList<>();
+        for (Map.Entry<String, Boolean> entry : group.getPerms().entrySet())
+            perms.add(entry.getValue() ? "" : "-" + entry.getKey());
+        try {
+            jedis.hset("group:" + name, "prefix", group.getPrefix());
+            jedis.hset("group:" + name, "ancestors", joiner.join(group.getAncestors()));
+            jedis.del("perms:" + group);
+            for (String perm : perms)
+                jedis.sadd("perms:" + group, perm);
+        } finally {
+            ConnectionManager.getPool().returnResource(jedis);
+        }
     }
 
     @Override
     public void savePlayerData(PlayerData playerData) {
-
+        Jedis jedis = ConnectionManager.getPool().getResource();
+        String name = playerData.getName().toLowerCase();
+        try {
+            jedis.hset("player:" + name, "prefix", playerData.getPrefix());
+            jedis.hset("player:" + name, "group", playerData.getGroup().getName().toLowerCase());
+        } finally {
+            ConnectionManager.getPool().returnResource(jedis);
+        }
     }
 
     @Override
@@ -27,7 +49,7 @@ public class RedisBackend implements Backend {
             if (jedis.exists("player:" + player))
                 return new PlayerData(player,
                         jedis.hget("player:" + player, "prefix"),
-                        rPermissions.getGroups().get(jedis.hget("player:" + player, "group"))
+                        rPermissions.getGroups().get(jedis.hget("player:" + player, "group").toLowerCase())
                 );
         } finally {
             ConnectionManager.getPool().returnResource(jedis);
@@ -36,24 +58,25 @@ public class RedisBackend implements Backend {
     }
 
     @Override
-    public Group loadGroup(String name) {
+    public Group loadGroup(String group) {
         Jedis jedis = ConnectionManager.getPool().getResource();
-        name = name.toLowerCase();
+        group = group.toLowerCase();
         try {
-            if (jedis.exists("group:" + name)) {
-                Set<String> permSet = jedis.smembers("perms:" + name);
+            if (jedis.exists("group:" + group)) {
+                Set<String> permSet = jedis.smembers("perms:" + group);
                 Map<String, Boolean> perms = new HashMap<>();
                 for (String perm : permSet)
                     perms.put(perm, !perm.startsWith("-"));
-                List<String> ancestorList = Arrays.asList(jedis.hget("group:" + name, "ancestors").split(","));
+                List<String> ancestorList = Arrays.asList(jedis.hget("group:" + group, "ancestors").split(","));
                 List<Group> ancestors = new ArrayList<>();
                 for (String ancestor : ancestorList)
-                    ancestors.add(loadGroup(ancestor));
-                return new Group(name, "", perms, ancestors);
+                    ancestors.add(rPermissions.getGroups().containsKey(ancestor.toLowerCase()) ?
+                            rPermissions.getGroups().get(ancestor.toLowerCase()) : loadGroup(ancestor));
+                return new Group(group, "", perms, ancestors);
             }
         } finally {
             ConnectionManager.getPool().returnResource(jedis);
         }
-        return new Group(name, "", new HashMap<String, Boolean>(), Collections.<Group>emptyList());
+        return new Group(group, "", new HashMap<String, Boolean>(), Collections.<Group>emptyList());
     }
 }
