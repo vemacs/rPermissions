f7537cc3b7349872e31022d5a033528723395e9a
vemacs <gentoo@outlook.com>
2014-02-22 12:16:51 -0700

    add player methods

--- src/main/java/me/vemacs/rperms/data/PlayerData.java
+++ src/main/java/me/vemacs/rperms/data/PlayerData.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NonNull;
 import me.vemacs.rperms.rPermissions;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachment;
 
 import java.util.ArrayList;
@@ -17,4 +18,21 @@ public class PlayerData {
     private String prefix;
     @NonNull
     private List<Group> groups = new ArrayList<>();
+
+    public void setup() {
+        Player player = getPlayer();
+        for (Group group : getGroups())  {
+            for (Group ancestor : group.getAncestors())
+                ancestor.attach(player);
+            group.attach(player);
+        }
+
+    }
+
+    public Player getPlayer() {
+        for (Player player : Bukkit.getOnlinePlayers())
+            if (player.getName().equalsIgnoreCase(name))
+                return player;
+        return null;
+    }
 }
