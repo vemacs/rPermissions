1582b80082d0d10acc69261397ce989cc97d3861
vemacs <gentoo@outlook.com>
2014-02-22 15:41:52 -0700

    prefix support for testing purposes

--- src/main/java/me/vemacs/rperms/backends/RedisBackend.java
+++ src/main/java/me/vemacs/rperms/backends/RedisBackend.java
@@ -1,6 +1,8 @@
 package me.vemacs.rperms.backends;
 
 import com.google.common.base.Joiner;
+import lombok.Data;
+import lombok.NonNull;
 import me.vemacs.rperms.data.Group;
 import me.vemacs.rperms.data.PlayerData;
 import me.vemacs.rperms.rPermissions;
@@ -10,6 +12,16 @@ import redis.clients.jedis.Jedis;
 import java.util.*;
 
 public class RedisBackend implements Backend {
+    private static String groupPrefix;
+    private static String playerPrefix;
+    private static String permPrefix;
+
+    public RedisBackend(String prefix) {
+        groupPrefix = prefix + "-group:";
+        playerPrefix = prefix + "-player:";
+        playerPrefix = prefix + "-perms:";
+    }
+
     private static final Joiner joiner = Joiner.on(",").skipNulls();
 
     public void saveGroup(Group group) {
@@ -19,11 +31,11 @@ public class RedisBackend implements Backend {
         for (Map.Entry<String, Boolean> entry : group.getPerms().entrySet())
             perms.add(entry.getValue() ? "" : "-" + entry.getKey());
         try {
-            jedis.hset("group:" + name, "prefix", group.getPrefix());
-            jedis.hset("group:" + name, "ancestors", joiner.join(group.getAncestors()));
-            jedis.del("perms:" + group);
+            jedis.hset(groupPrefix + name, "prefix", group.getPrefix());
+            jedis.hset(groupPrefix + name, "ancestors", joiner.join(group.getAncestors()));
+            jedis.del(permPrefix + group);
             for (String perm : perms)
-                jedis.sadd("perms:" + group, perm);
+                jedis.sadd(permPrefix + group, perm);
         } finally {
             ConnectionManager.getPool().returnResource(jedis);
         }
@@ -34,8 +46,8 @@ public class RedisBackend implements Backend {
         Jedis jedis = ConnectionManager.getPool().getResource();
         String name = playerData.getName().toLowerCase();
         try {
-            jedis.hset("player:" + name, "prefix", playerData.getPrefix());
-            jedis.hset("player:" + name, "group", playerData.getGroup().getName().toLowerCase());
+            jedis.hset(playerPrefix + name, "prefix", playerData.getPrefix());
+            jedis.hset(playerPrefix + name, "group", playerData.getGroup().getName().toLowerCase());
         } finally {
             ConnectionManager.getPool().returnResource(jedis);
         }
@@ -46,10 +58,10 @@ public class RedisBackend implements Backend {
         Jedis jedis = ConnectionManager.getPool().getResource();
         player = player.toLowerCase();
         try {
-            if (jedis.exists("player:" + player))
+            if (jedis.exists(playerPrefix + player))
                 return new PlayerData(player,
-                        jedis.hget("player:" + player, "prefix"),
-                        rPermissions.getGroups().get(jedis.hget("player:" + player, "group").toLowerCase())
+                        jedis.hget(playerPrefix + player, "prefix"),
+                        rPermissions.getGroups().get(jedis.hget(playerPrefix + player, "group").toLowerCase())
                 );
         } finally {
             ConnectionManager.getPool().returnResource(jedis);
@@ -62,12 +74,12 @@ public class RedisBackend implements Backend {
         Jedis jedis = ConnectionManager.getPool().getResource();
         group = group.toLowerCase();
         try {
-            if (jedis.exists("group:" + group)) {
-                Set<String> permSet = jedis.smembers("perms:" + group);
+            if (jedis.exists(groupPrefix + group)) {
+                Set<String> permSet = jedis.smembers(permPrefix + group);
                 Map<String, Boolean> perms = new HashMap<>();
                 for (String perm : permSet)
                     perms.put(perm, !perm.startsWith("-"));
-                List<String> ancestorList = Arrays.asList(jedis.hget("group:" + group, "ancestors").split(","));
+                List<String> ancestorList = Arrays.asList(jedis.hget(groupPrefix + group, "ancestors").split(","));
                 List<Group> ancestors = new ArrayList<>();
                 for (String ancestor : ancestorList)
                     ancestors.add(rPermissions.getGroups().containsKey(ancestor.toLowerCase()) ?
